0a998ff21510f9dd1a61466785e519054d991023
Currents <currentsaisyah@gmail.com>
2025-05-14 12:40:48 +0700

    Zaaramedia for team

--- src/com/jdialog/FormGenerateCard.java
+++ src/com/jdialog/FormGenerateCard.java
@@ -19,6 +19,7 @@ import com.service.ServicePelanggan;
 import com.tablemodel.TableModelPelanggan;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -30,6 +31,7 @@ import javax.swing.JOptionPane;
 
 public class FormGenerateCard extends javax.swing.JDialog {
 
+    private boolean isFront = true;
     private TableModelPelanggan tblModel = new TableModelPelanggan();
     private ServicePelanggan servis = new PelangganDAO();
     private ModelPelanggan pelanggan;
@@ -64,6 +66,7 @@ public class FormGenerateCard extends javax.swing.JDialog {
         jPanel3 = new javax.swing.JPanel();
         lbCard = new javax.swing.JLabel();
         btnPrint = new javax.swing.JButton();
+        btnPrint1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -146,6 +149,16 @@ public class FormGenerateCard extends javax.swing.JDialog {
             }
         });
 
+        btnPrint1.setBackground(new java.awt.Color(230, 209, 242));
+        btnPrint1.setFont(new java.awt.Font("SansSerif", 1, 18)); // NOI18N
+        btnPrint1.setForeground(new java.awt.Color(0, 0, 0));
+        btnPrint1.setText("FLIP");
+        btnPrint1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPrint1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -158,13 +171,18 @@ public class FormGenerateCard extends javax.swing.JDialog {
                         .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addContainerGap())
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(txtID)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(btnPrint, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(btnGenerate, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 164, Short.MAX_VALUE))
-                            .addComponent(jLabel9, javax.swing.GroupLayout.DEFAULT_SIZE, 167, Short.MAX_VALUE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(btnPrint1))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtID)
+                                    .addComponent(btnGenerate, javax.swing.GroupLayout.DEFAULT_SIZE, 179, Short.MAX_VALUE)
+                                    .addComponent(btnPrint, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGap(0, 8, Short.MAX_VALUE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18))))
         );
@@ -175,7 +193,7 @@ public class FormGenerateCard extends javax.swing.JDialog {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel9)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -183,9 +201,11 @@ public class FormGenerateCard extends javax.swing.JDialog {
                         .addGap(18, 18, 18)
                         .addComponent(btnGenerate, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(btnPrint, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(btnPrint, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnPrint1, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(26, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -207,7 +227,7 @@ public class FormGenerateCard extends javax.swing.JDialog {
     if (!validasiInput()) {
         return; 
     }
-    image = generateCardImage();
+    image = generateCardFront();
     ImageIcon icon = new ImageIcon(image);
     lbCard.setIcon(icon);
     }//GEN-LAST:event_btnGenerateActionPerformed
@@ -219,6 +239,17 @@ public class FormGenerateCard extends javax.swing.JDialog {
     }
         printCard(image);
     }//GEN-LAST:event_btnPrintActionPerformed
+
+    private void btnPrint1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPrint1ActionPerformed
+        if (isFront) {
+            image = generateCardBack();  
+        } else {
+            image = generateCardFront(); 
+        }
+
+        lbCard.setIcon(new ImageIcon(image));
+        isFront = !isFront; 
+    }//GEN-LAST:event_btnPrint1ActionPerformed
     
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -238,6 +269,7 @@ public class FormGenerateCard extends javax.swing.JDialog {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnGenerate;
     private javax.swing.JButton btnPrint;
+    private javax.swing.JButton btnPrint1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel9;
@@ -249,44 +281,99 @@ public class FormGenerateCard extends javax.swing.JDialog {
     private javax.swing.JTextField txtID;
     // End of variables declaration//GEN-END:variables
     
-   private BufferedImage generateCardImage() {
+    private BufferedImage generateCardFront() {
         int width = 400;
         int height = 250;
         BufferedImage cardImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics2D g2d = cardImage.createGraphics();
 
-        Color startColor = new Color(173, 216, 230); 
-        Color endColor = new Color(100, 200, 255);   
+        Color startColor = new Color(173, 216, 230);
+        Color endColor = new Color(100, 200, 255);
         GradientPaint gp = new GradientPaint(0, 0, startColor, width, height, endColor);
         g2d.setPaint(gp);
         g2d.fillRect(0, 0, width, height);
 
-        g2d.setColor(Color.BLACK);
-        g2d.setFont(new Font("SansSerif", Font.BOLD, 18));
-        g2d.drawString("Member Card", 140, 30);
-        g2d.setFont(new Font("SansSerif", Font.BOLD, 16));
-        g2d.drawString("Nama Pelanggan: " + pelanggan.getNamaPelanggan(), 40, 60);
-        g2d.drawString("ID Pelanggan: " + pelanggan.getIdPelanggan(), 40, 80);
-       
-        BufferedImage barcodeImage = null;
-
         try {
-            Barcode barcode = BarcodeFactory.createCode128(String.valueOf(pelanggan.getKodeRfid()));
-            barcodeImage = BarcodeImageHandler.getImage(barcode);
-        } catch (Exception e) {
+            BufferedImage logo = ImageIO.read(getClass().getResource("/com/icon/logo_ZaaraMedia.png")); 
+            if (logo != null) {
+                int logoX = (width - logo.getWidth()) / 2;
+                int logoY = (height - logo.getHeight()) / 2 - 30; 
+                g2d.drawImage(logo, logoX, logoY, null);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        if (barcodeImage != null) {
-            int barcodeX = (width - barcodeImage.getWidth()) / 2;
-            int barcodeY = 100;
-            g2d.drawImage(barcodeImage, barcodeX, barcodeY, null);
-        }
+        g2d.setColor(Color.BLACK);
+        g2d.setFont(new Font("SansSerif", Font.PLAIN, 12));
+        FontMetrics fm1 = g2d.getFontMetrics();
+        String line1 = "AL-QUR'AN DAN BUKU ISLAM";
+        int line1Width = fm1.stringWidth(line1);
+        g2d.drawString(line1, (width - line1Width) / 2, height / 2 + 30);
+
+        g2d.setFont(new Font("SansSerif", Font.BOLD, 30));
+        FontMetrics fm2 = g2d.getFontMetrics();
+        String line2 = "MEMBERSHIP CARD";
+        int line2Width = fm2.stringWidth(line2);
+        g2d.drawString(line2, (width - line2Width) / 2, height / 2 + 70);
+
         g2d.dispose();
         return cardImage;
     }
-
     
+        private BufferedImage generateCardBack() {
+            int width = 400;
+            int height = 250;
+            BufferedImage cardBack = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g2d = cardBack.createGraphics();
+        
+            Color startColor = new Color(173, 216, 230);
+            Color endColor = new Color(100, 200, 255);
+            GradientPaint gp = new GradientPaint(0, 0, startColor, width, height, endColor);
+            g2d.setPaint(gp);
+            g2d.fillRect(0, 0, width, height);
+            
+            g2d.setColor(Color.BLACK);
+            g2d.setFont(new Font("SansSerif", Font.PLAIN, 14));
+            g2d.drawString("Syarat dan Ketentuan Berlaku:", 20, 40);
+            g2d.drawString("- Kartu ini tidak dapat dipindahtangankan", 20, 60);
+            g2d.drawString("- Hubungi CS jika kartu hilang", 20, 80);
+
+            try {
+                Barcode barcode = BarcodeFactory.createCode128(String.valueOf(pelanggan.getKodeRfid()));
+                BufferedImage barcodeImage = BarcodeImageHandler.getImage(barcode);
+
+                if (barcodeImage != null) {
+                    int barcodeX = 20;
+                    int barcodeY = height - barcodeImage.getHeight() - 50;
+                    g2d.drawImage(barcodeImage, barcodeX, barcodeY, null);
+
+                    int textY = barcodeY - 10;
+                    g2d.setColor(Color.BLACK);
+                    g2d.setFont(new Font("SansSerif", Font.PLAIN, 12));
+
+                    FontMetrics fm = g2d.getFontMetrics();
+                    String line1 = "\u260E 0851 9728 9505";
+                    String line2 = "\u0040 zaaramedia";
+                    String line3 = "\u2302 Jl. Pattimura Karanganyar, Ambulu, Jember";
+
+                    int textX1 = (width - fm.stringWidth(line1)) / 2;
+                    int textX2 = (width - fm.stringWidth(line2)) / 2;
+                    int textX3 = (width - fm.stringWidth(line3)) / 2;
+
+                    g2d.drawString(line1, textX1, textY - 30);
+                    g2d.drawString(line2, textX2, textY - 15);
+                    g2d.drawString(line3, textX3, textY);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            g2d.dispose();
+            return cardBack;
+        }
+
+
     private void printCard(BufferedImage image) {
         PrinterJob printerJob = PrinterJob.getPrinterJob();
         printerJob.setPrintable(new Printable() {
